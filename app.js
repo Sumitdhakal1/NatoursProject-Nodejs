50e5f475aa712b2ad680719eb16d9e5eb211d98e
Sumit Dhakal <sumit@Sumits-MacBook-Pro.local>
2024-03-03 17:06:58 +0545

    handeling post request

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 const fs = require('fs')
+
+app.use(express.json()); //express.json is middelware
 // app.get('/', (req, res)=>{
 
 // res.status(200).json({message:'hello from the server', app:'Natours'});
@@ -13,7 +15,7 @@ const fs = require('fs')
 //     res.send('you can  send post message')
 // })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
 
@@ -23,8 +25,26 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
-    })
-})
+    });
+});
+
+app.post('/api/v1/tours', (req, res) =>{
+//   console.log(req.body);
+
+  const newId = tours[tours.length-1].id+1;
+  const newTour = Object.assign({id :newId}, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
+    // 201 stands for created
+   res.status(201).json({
+    status:'success',
+    data:{
+        tour: newTour
+    }
+   })
+  })
+});
 
 const port = 3000
 app.listen(port, () => {
